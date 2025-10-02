0abbbb2f1f56d6d868cd7f1eb35de55c2c340b37
capricorn7000 <alexander.starcevic@gmail.com>
2025-10-02 14:49:25 +0200

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,44 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5tsp01Xe21y":
+        Script1();
+        break;
+      case "6LJj7pcMlVY":
+        Script2();
+        break;
+      case "5aYYKV98uHE":
+        Script3();
+        break;
+      case "5Zj7Z02si7p":
+        Script4();
+        break;
+      case "60mr0VweBwP":
+        Script5();
+        break;
+      case "5ivBoAcb4Sb":
+        Script6();
+        break;
+      case "5xOnkAPYDRt":
+        Script7();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
